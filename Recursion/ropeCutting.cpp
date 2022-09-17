e598a7c1a1a0f0545ac02244d4f6e5d044255bc2
Sukhvinder Singh <sukhvindersingh@Sukhvinders-MacBook-Air.local>
2022-09-17 13:26:23 -0500

    rope cutting done

--- Recursion/ropeCutting.cpp
+++ Recursion/ropeCutting.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
-#include<cmath>
 
 using namespace std;
 
 int ropeCutting(int n, int a, int b, int c){
-    if(n==0) return 0;
+    if(n == 0) return 0;
     else if(n < 0) return -1;
-    int a1 = ropeCutting(n-a, a, b, c);
-    int b1 = ropeCutting(n-b, a, b, c);
-    int c1 = ropeCutting(n-c, a, b, c);
-    int res = max(a1,b1);
-    res = max(res,c1);
-    if(res == -1) return-1;
+
+    int res = max(ropeCutting(n-a, a, b, c), ropeCutting(n-b, a, b, c));
+    res = max(res, ropeCutting(n-c, a, b, c));
+
+    if(res == -1) return -1;
     return res+1;
 }
+
+
 
 int main(){
     cout<<"Rope of length 5, a = 2, b = 5, c = 1: "<<ropeCutting(5, 2, 5, 1)<<endl;
