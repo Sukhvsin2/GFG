b8843a86a66eab7881a23b693448a7916669ab93
Sukhvinder Singh <sukhvindersingh@Sukhvinders-MacBook-Air.local>
2022-09-17 19:35:59 -0500

    can't get the josephus problem

---
+++ Recursion/josephusProblem.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int jos(int n, int k){
+    if(n==1) return 0;
+     
+    return (jos(n-1, k) + k)%n;
+}
+
+int main(){
+    cout<<"Position for 4, 2: "<<jos(4, 2)<<endl;
+    return 0;
+}
